96a7f0f6b1f13cdcbbd8b703228c596d46639529
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-05-24 21:56:41 +0530

    First Commit

---
+++ javabasics.java
@@ -0,0 +1,11 @@
+public class javabasics{
+    public static void main(String[] args) {
+        // System.out.println("hello champion!!");
+        // System.out.println("hello champion!!");
+        // System.out.println("hello champion!!");
+
+
+        
+
+    }
+}
