69b01a679ac2c2d2724c6f935010f6cb85637080
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-05-26 11:20:18 +0530

    input

--- javabasics.java
+++ javabasics.java
@@ -1,11 +1,16 @@
+import java.util.*;
 public class javabasics{
     public static void main(String[] args) {
         // System.out.println("hello champion!!");
         // System.out.println("hello champion!!");
         // System.out.println("hello champion!!");
 
+        Scanner sc =new Scanner(System.in);
+        // String input=sc.nextLinet();
+        // System.out.println(input);
 
-        
 
+        int number = sc.nextInt();
+        System.out.println(number);
     }
 }
