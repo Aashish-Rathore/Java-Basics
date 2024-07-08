d442b615e0dbed2d0b75722cf93618531895da62
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-07-08 21:08:21 +0530

    add file

---
+++ Functions And Methods/Overloading.java
@@ -0,0 +1,18 @@
+
+public class Overloading {
+
+
+
+    //func to calc sum f 2 nums
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    public static int sum (int a, int b , int c){
+        return a+b+c;
+    }
+    public static void main (String[] args) {
+        System.out.println(sum(4, 5));
+        System.out.println(sum(5, 2, 1));
+    }
+}
