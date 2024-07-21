f4237e285017aefa7270571600a70e8bd071c8fb
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-07-21 22:19:10 +0530

    add files

---
+++ OOPS/Package.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+import java.util.*;
+// package is a group of familar types of classes, interfaces 
+// and sub-packages.
+public class Package {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        //Inbuild package
+        int a=sc.nextInt(); 
+    
+        System.out.println(a); 
+    } 
+}
--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -3,26 +3,44 @@ package OOPS;
 public class Polymorphism {
     // Compile time polymorphism (FUNCTION OVERLOADING)
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.sum(1, 5));
-        System.out.println(calc.sum((float)2.4, (float)5.6));
-        System.out.println(calc.sum(4, 5));
+        // Calculator calc = new Calculator();
+        // System.out.println(calc.sum(1, 5));
+        // System.out.println(calc.sum((float)2.4, (float)5.6));
+        // System.out.println(calc.sum(4, 5));
         
+
+
+        // Run time polymorphism (FUNCTION OVERRIDING)  
+        Animal A =new Animal();
+        A.eat();
+
     }
 }
 
-class Calculator{
-    int sum(int a, int b){
-        return a+b;
+// class Calculator{
+//     int sum(int a, int b){
+//         return a+b;
+//     }
+
+
+//     float sum(float a , float b){
+//         return a+b;
+//     }
+
+//     int sum(int a , int b, int c){
+//         return a+b+c;
+//     }
+
+// }
+
+class Animal{
+    void eat(){
+        System.out.println("eats anything");
+    }
+  }
+  class Deer extends Animal{
+    void eat(){
+        System.out.println("eats grass");
     }
 
-
-    float sum(float a , float b){
-        return a+b;
-    }
-
-    int sum(int a , int b, int c){
-        return a+b+c;
-    }
-
-}
+  }
